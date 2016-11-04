19c3a74f76ca5cc2563fde5f388ba9dabf35a81f
bstone17 <bstone17@punahou.edu>
2016-11-03 15:28:47 -1000

    Basic spawn point for player controlled sprite
    Mostly worked on surface issues like where to spawn the dot. But added
    in some of the basic spawn point.

--- Meggy_ChasingChaos.ino
+++ Meggy_ChasingChaos.ino
@@ -1,11 +1,27 @@
 // Meggy_ChasingChaos By: Ikaika Stone
 
+#include <MeggyJrSimple.h>    // Required code, line 1 of 2.
+
+struct Point
+{  
+  int x;
+  int y;
+};
+
+Point pc = {4,5};   // Spawn point for player controlled sprite
+
 void setup() {
-  // put your setup code here, to run once:
-
+{  
+   MeggyJrSimpleSetup();      // Required code, line 2 of 2.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{              // says function definition is not allowed here before '{' token
+  DrawPx(pc.x,pc.y,Green);    // Draw player's dot in green
+  DisplaySlate();
+  ClearSlate();
 }
+
+
+  
+
