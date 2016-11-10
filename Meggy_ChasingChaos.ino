462a8d898e2b619f098790c2c1da45a3cdcf9be9
bstone17 <bstone17@punahou.edu>
2016-11-09 15:30:23 -1000

    Adding in enemy sprites spawn
    I sat down for most of my time with you today. I have all the code, but
    have not inputted it yet on how to correctly spawn enemies without them
    randomly spawning on top of the player controlled sprite. Will be
    adding this soon while it’s fresh in my head

--- Meggy_ChasingChaos.ino
+++ Meggy_ChasingChaos.ino
@@ -8,16 +8,30 @@ struct Point
   int y;
 };
 
+struct Enemy        
+{
+  int x;
+  int y;
+};
+
+Enemy e1 = {0,0}
+Enemy e2 = {0,0}
+Enemy e3 = {0,0}
+Enemy enemies[3] = {e1,e2,e3};
+
 Point pc = {4,5};   // Spawn point for player controlled sprite
-int direction = 0;
+int px = 0
+int py = 0
 
 void setup() 
 {  
    MeggyJrSimpleSetup();      // Required code, line 2 of 2.
+   Serial.begin(9600);
 }
 
 void loop() 
 {              // says function definition is not allowed here before '{' token
+  Serial.print("functioncalled");
   DrawPx(pc.x,pc.y,Green);    // Draw player's dot in green
   DisplaySlate();
   ClearSlate();
